992d1f266a65f5eb41e208561c9dbb9960baab90
iamtash <natashachernis@gmail.com>
2019-10-28 15:39:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+  static capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }
+
+  static sanitize(text) {
+   let match = text.match(/[\w\s-']+/g)
+  
+   return match ? match.join('') : ""
+  }
+
+  static titleize(text) {
+    const keepLowerCase = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const splitText = text.split(' ')
+    const temp = [this.capitalize(splitText[0]), ...splitText.slice(1)]
+    return temp.map(word => {
+      return keepLowerCase.includes(word) ? word : this.capitalize(word)
+    }).join(' ')
+  }
 }
