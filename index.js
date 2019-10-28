91ef08d2801f7424de47e6128749fba0c4418887
Michael Schiumo <mschiumo18@gmail.com>
2019-10-28 19:29:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^\w\s-']+/g, '')
+  }
+
+  static titleize(string) {
+  //   if (string === "a" || "the" || "an" || "but" || "of"
+  //       || "and" || "for" || "at" || "by" || "from") {
+  //     return string
+  //   }
+  // }\\
+    let stringArray = string.split(' ');
+    let sentence = [];
+    let exceptions = ["a", "the", "but", "of", "and", "for", "at", "by", "from", "an"]
+
+    // for (let i = 0; i < stringArray.length; i++) {
+    //     if (stringArray[i]) {
+    //       return stringArray[i].charAt(0).toUpperCase() + string.slice(1)
+    //     }
+    // }
+      for (let i = 0; i < stringArray.length; i++) {
+
+        if (i === 0) {
+        sentence.push(this.capitalize(stringArray[i]))
+      } else if (exceptions.includes(stringArray[i])) {
+        sentence.push(stringArray[i])
+      } else {
+          sentence.push(this.capitalize(stringArray[i]))
+        }
+      }
+    return sentence.join(' ')
+  }
 }
