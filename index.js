80afad2465ab15c80c565ee3a62e05d156192e73
Danieleclima <daniel.echuaca.lima@gmail.com>
2019-11-10 19:55:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 class Formatter {
 
   static capitalize(string){
-    string.charAt(0).toUpperCase() + string.slice(1)
+   return string.charAt(0).toUpperCase() + string.slice(1)
   }
 
   static sanitize(string){
-    return string.replace(/[^A-Za-z0-9-']+/g, '');
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
   }
 
   static titleize(string){
-    if(!string.split) return string;
+    // if(!string.split) return string;
+    let array = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
     let _titleizeWord = function(word) {
+          if(!array.includes(word)){
             return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-        },
+    }else{
+      return word
+    }},
         result = [];
-    string.split(" ").forEach(function(w) {
+    string.split(" ").forEach(function(w, index) {
+        if(index == 0){
+          result.push(this.capitalize(w))
+        } else{
         result.push(_titleizeWord(w));
-    });
+        }
+    }, this);
     return result.join(" ");
 
   }
